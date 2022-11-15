edfb0c8e157f81fd4759f8e31039334c5478f7af
sidnnov <sidnnov@gmail.com>
2022-11-15 12:25:27 +0100

    Update generate.py, test_generate_diff.py

--- gendiff/generate.py
+++ gendiff/generate.py
@@ -1,29 +1,9 @@
-import json
+from gendiff.parser import parser
 
-
-# def generate_diff(puth_file1, puth_file2):
-#     dict_1 = make_sorted_dict_from_file(puth_file1)
-#     dict_2 = make_sorted_dict_from_file(puth_file2)
-#     result = ''
-
-#     for key, val in dict_1.items():
-#         if key in dict_2:
-#             if val == dict_2[key]:
-#                 result += f'    {key}: {val}\n'
-#             else:
-#                 result += f'  - {key}: {val}\n'
-#                 result += f'  + {key}: {dict_2[key]}\n'
-#         if key not in dict_2:
-#             result += f'  - {key}: {val}\n'
-
-#     for key, val in dict_2.items():
-#         if key not in dict_1:
-#             result += f'  + {key}: {val}\n'
-#     result = '{\n' + result + '}'
 
 def generate_diff(puth_file1, puth_file2):
-    dict_1 = make_sorted_dict_from_file(puth_file1)
-    dict_2 = make_sorted_dict_from_file(puth_file2)
+    dict_1 = parser(puth_file1)
+    dict_2 = parser(puth_file2)
     keys = dict_1.keys() | dict_2.keys()
     result = ''
 
@@ -40,45 +20,3 @@ def generate_diff(puth_file1, puth_file2):
     result = '{\n' + result + '}'
 
     return result
-
-    # list1 = []
-    # with open(puth_file1) as file1:
-    #     dict_1 = json.loads(file1.read())
-    # # print(dict(sorted(dict_1.items())))
-    #
-    # for key, val in dict_1.items():
-    #     if type(val) == bool:
-    #         dict_1[key] = str(val).lower()
-    #
-    # list1 = [': '.join([str(key), str(val)]) for key, val in dict_1.items()]
-    #
-    # with open(puth_file2) as file2:
-    #     dict_2 = json.loads(file2.read())
-    # for key, val in dict_2.items():
-    #     if type(val) == bool:
-    #         dict_2[key] = str(val).lower()
-    # return sorted(list1), dict_2
-
-
-def make_sorted_dict_from_file(puth_file):
-    with open(puth_file) as file:
-        dictionary = json.loads(file.read())
-    sorted_dict = dict(sorted(dictionary.items()))
-
-    for key, val in sorted_dict.items():
-        if type(val) == bool:
-            sorted_dict[key] = str(val).lower()
-
-    # for key, val in dict.items():
-    #     if type(val) == bool:
-    #         dict[key] = str(val).lower()
-    # list_values = [
-    #     ': '.join([str(key), str(val)]) for key, val in dict.items()
-    #     ]
-
-    return sorted_dict
-
-
-# puth_file1 = 'path_files/file1.json'
-# puth_file2 = 'path_files/file2.json'
-# print(generate_diff(puth_file1, puth_file2))
--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,7 +1,17 @@
-from gendiff.generate import generate_diff
+from gendiff import generate_diff
+from gendiff.parser import parser
 
-PUTH_FILE1 = 'tests/fixtures/file1.json'
-PUTH_FILE2 = 'tests/fixtures/file2.json'
+
+PUTH_JSON_FILE1 = 'tests/fixtures/file1.json'
+PUTH_JSON_FILE2 = 'tests/fixtures/file2.json'
+PUTH_YAML_FILE1 = 'tests/fixtures/file1.yaml'
+PUTH_YAML_FILE2 = 'tests/fixtures/file2.yml'
+CORRECT_DICT = {
+    'follow': 'false',
+    'host': 'hexlet.io',
+    'proxy': '123.234.53.22',
+    'timeout': 50
+}
 
 
 with open('tests/fixtures/expected.txt') as file:
@@ -9,4 +19,10 @@ with open('tests/fixtures/expected.txt') as file:
 
 
 def test_generate_diff():
-    assert generate_diff(PUTH_FILE1, PUTH_FILE2) == expected
+    assert generate_diff(PUTH_JSON_FILE1, PUTH_JSON_FILE2) == expected
+    assert generate_diff(PUTH_YAML_FILE1, PUTH_YAML_FILE2) == expected
+
+
+def test_parser():
+    assert parser(PUTH_JSON_FILE1) == CORRECT_DICT
+    assert parser(PUTH_YAML_FILE1) == CORRECT_DICT
